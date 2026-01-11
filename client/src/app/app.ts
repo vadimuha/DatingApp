881c116ba08403dd979e064961b2b248797c1542
Vadym Hryhorchuk <vadimuha13@gmail.com>
2026-01-11 16:23:01 +0200

    Section 3

---
+++ client/src/app/app.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit{
+
+  protected title = 'client';
+  users:any;
+  http = inject(HttpClient);
+    ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/users").subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log("Fetched users"),
+    })
+  }
+}
